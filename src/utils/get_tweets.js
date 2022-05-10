6485368a5a65658979820f2b4c6b519266036265
Dominic <dominicvana@gmail.com>
2022-05-10 02:11:26 -0500

    Refactor: variable naming

--- src/utils/get_tweets.js
+++ src/utils/get_tweets.js
@@ -4,12 +4,12 @@ import fetch from 'node-fetch';
 dotenv.config();
 
 const { TWITTER_BEARER_TOKEN } = process.env;
-
-const auth = {
+const AUTH = {
   headers: {
     authorization: `Bearer ${TWITTER_BEARER_TOKEN}`,
   },
 };
+const BASE_URL = 'https://api.twitter.com/2/users';
 
 /**
  * Get twitter ID and display name based on a username/handle
@@ -21,9 +21,9 @@ const auth = {
  */
 const getUserData = async username => {
   try {
-    const url = `https://api.twitter.com/2/users/by/username/${username}?user.fields=name,username,id,verified,profile_image_url,location,description,public_metrics`;
+    const url = `${BASE_URL}/by/username/${username}?user.fields=name,username,id,verified,profile_image_url,location,description,public_metrics`;
 
-    const { data } = await fetch(url, auth).then(r => r.json());
+    const { data } = await fetch(url, AUTH).then(r => r.json());
 
     return data;
   } catch (error) {
@@ -43,10 +43,10 @@ const getUserData = async username => {
 const getUserTweets = async username => {
   const user = await getUserData(username);
 
-  const url = `https://api.twitter.com/2/users/${user.id}/tweets?tweet.fields=id,created_at,text,author_id,public_metrics`;
+  const url = `${BASE_URL}/${user.id}/tweets?tweet.fields=id,created_at,text,author_id,public_metrics`;
 
   try {
-    const { data: tweets } = await fetch(url, auth).then(r => r.json());
+    const { data: tweets } = await fetch(url, AUTH).then(r => r.json());
 
     return { user, tweets };
   } catch (error) {
