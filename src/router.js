fa0ca03197e5aa3409ebc9b77437efcefb36130f
Dominic <dominicvana@gmail.com>
2022-05-09 20:46:54 -0500

    Chore: add nodemon config

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-// import reqLogger from '@notthedom/logging-express';
+import reqLogger from '@notthedom/logging-express';
 import getUserTweets from './utils/get_tweets.js';
 import filterUserTweets from './utils/filter_tweets.js';
 
@@ -8,12 +8,8 @@ const router = Router();
 // mounted at /api in index.js
 router
   .route('/tweets/:username?/:topics?/:numResults?')
-  .get(async (req, res) => {
-    const {
-      username = 'elonmusk',
-      topics = null,
-      numResults = 10,
-    } = req.params;
+  .get(reqLogger, async (req, res) => {
+    const { username = 'balajis', topics = null, numResults = 10 } = req.params;
 
     const { user, tweets } = await getUserTweets(username, numResults);
 
